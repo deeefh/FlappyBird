b4c5932d0c44046319d74ce049ada50705fdfcc6
deeefh <hungtrantuan20080@gmail.com>
2024-08-15 14:39:37 +0700

    update

--- flappybird.cpp
+++ flappybird.cpp
@@ -14,7 +14,7 @@
 #define PIPE_DIF 45
 
 using namespace std;
-//		cout<<"±±±±±±±±±";
+//		cout<<"000000000";
  
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 COORD CursorPosition;
@@ -48,16 +48,16 @@ void setcursor(bool visible, DWORD size)
 void drawBorder(){ 
 	
 	for(int i=0; i<SCREEN_WIDTH; i++){
-		gotoxy(i,0); cout<<"±";
-		gotoxy(i,SCREEN_HEIGHT); cout<<"±";
+		gotoxy(i,0); cout<<"0";
+		gotoxy(i,SCREEN_HEIGHT); cout<<"0";
 	}
 	
 	for(int i=0; i<SCREEN_HEIGHT; i++){
-		gotoxy(0,i); cout<<"±";
-		gotoxy(SCREEN_WIDTH,i); cout<<"±";
+		gotoxy(0,i); cout<<"0";
+		gotoxy(SCREEN_WIDTH,i); cout<<"0";
 	}
 	for(int i=0; i<SCREEN_HEIGHT; i++){
-		gotoxy(WIN_WIDTH,i); cout<<"±";
+		gotoxy(WIN_WIDTH,i); cout<<"0";
 	}
 }
 void genPipe(int ind){
